452fac61f519c15110b4f6749a45520f8b93c48a
Zhanna <zhanna.libman@gmail.com>
2017-03-25 00:09:13 +0300

    tree and folder version 2

--- src/app/folder/folder.component.ts
+++ src/app/folder/folder.component.ts
@@ -11,17 +11,29 @@ export class FolderComponent implements OnInit {
 
   private folder: Folder;
   @Input() id?: string;
+  @Input() expandable: boolean = false;
   private expanded: boolean = false;
+  private expandSign: string = "";
 
   constructor(private fileSystemService: FileSystemService) {
   }
 
   ngOnInit() {
+    if (this.expandable) {
+      this.expandSign = "+";
+    }
     this.fileSystemService.getItemById(this.id).subscribe((response) => {
       this.folder = response.item;
     }, (err) => {
       console.log(err);
     });
+  }
+
+  onFolderImageClick(){
+    if (this.expandable) {
+      this.expanded=!this.expanded;
+      this.expandSign = this.expanded ? "-" : "+";
+    }
   }
 
 }
