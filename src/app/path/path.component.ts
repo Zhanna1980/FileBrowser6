d5dd8cdcb4c47bbbece682103a2bd48486ff7609
Zhanna <zhanna.libman@gmail.com>
2017-04-10 17:27:37 +0300

    current item name in path component

--- src/app/path/path.component.ts
+++ src/app/path/path.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Folder} from "../folder";
+import {File} from "../file";
+import {Subscription} from "rxjs";
+import {AppService} from "../app-service.service";
 
 @Component({
   selector: 'path',
   templateUrl: './path.component.html',
   styleUrls: ['./path.component.scss']
 })
-export class PathComponent implements OnInit {
 
-  constructor() { }
+export class PathComponent implements OnInit, OnDestroy {
+  private currentItem: File | Folder;
+  private onGotCurrentItem: Subscription;
+  private onGotItem: Subscription;
+
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
+    this.onGotCurrentItem = this.appService.onCurrentItemChanged.subscribe((item: Folder | File) => {
+      this.currentItem = item;
+    });
+    this.onGotItem = this.appService.onGotItem.subscribe((item: Folder | File) => {
+      if (item._id == this.currentItem._id) {
+        this.currentItem = item;
+      }
+    });
+  }
+  ngOnDestroy() {
+    if (this.onGotCurrentItem) {
+      this.onGotCurrentItem.unsubscribe();
+    }
+    if (this.onGotItem) {
+      this.onGotItem.unsubscribe();
+    }
   }
 
 }
